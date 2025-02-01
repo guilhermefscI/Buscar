3154be3ee3dd6e76bb508a9cfe1aa8b732048c63
Lizard <132412348+guilhermefscI@users.noreply.github.com>
2025-02-01 17:41:37 -0300

    Add files via upload

---
+++ BackEnd/cadastro_BD.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$dtnasc = $_POST['dtnasc'];
+$telefone = $_POST['telefone'];
+
+$logado = false;
+
+$con = mysqli_connect('localhost', "root", "projetec", "projetec");
+$query = "INSERT INTO usuario (nome, email, senha, dataN, telefone) values ('$nome', '$email', '$senha', '$dtnasc', '$telefone')";
+
+$sql = "SELECT email FROM usuario WHERE email = ?";
+$stmt = mysqli_prepare($con, $sql);
+
+if ($stmt) {
+
+    mysqli_stmt_bind_param($stmt, 's', $email);
+
+    mysqli_stmt_execute($stmt);
+
+    $resultado = mysqli_stmt_get_result($stmt);
+
+    if (mysqli_num_rows($resultado) == 0) {
+        $row = mysqli_fetch_assoc($resultado);
+
+        $_SESSION["nome"] = $_POST["nome"];
+        $_SESSION["senha"] = $_POST["senha"];
+        $_SESSION["email"] = $_POST["email"];
+        $_SESSION["telefone"] = $_POST["telefone"];
+        $_SESSION["dtnasc"] = $_POST["dtnasc"];
+
+        $logado = true;
+        $_SESSION['logado'] = $logado;
+
+        mysqli_query($con, $query);
+
+        header('Location: ../FrontEnd/Home/home.php');
+    } else {
+        echo "<script>alert('Email inserido já esta cadastrado');</script>";
+        echo "<script>window.location.href = '../FrontEnd/cadastro/cadastro.php';</script>";
+    }
+
+    mysqli_stmt_close($stmt);
+} else {
+    echo "<script>alert('Erro');</script>";
+    echo "<script>window.location.href = '../FrontEnd/cadastro/cadastro.php';</script>";
+}
+
+mysqli_close($con);

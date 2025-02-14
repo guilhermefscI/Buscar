8d450ca6a99af1104d64e383018630c55f55cc33
Lizard <132412348+guilhermefscI@users.noreply.github.com>
2025-02-14 11:01:57 -0300

    Delete FrontEnd directory

--- FrontEnd/Perfil/sair.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-
-session_destroy();
-
-header('Location: http://localhost/ProjetecT/FrontEnd/Perfil/perfil.php');
--- FrontEnd/Página do Onibus/4988/Nav.js
+++
@@ -1,7 +0,0 @@
-const icone = document.querySelector(".icone");
-const opcoes = document.querySelector(".opcoes");
-
-icone.addEventListener("click", () =>{
-    icone.classList.toggle('active')
-    opcoes.classList.toggle('active')
-})
